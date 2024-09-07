3129b7b176517de93d1b4728ab31b284de80ad73
Giorgi Abesadze <abesdzegiorgi@gmail.com>
2024-09-07 18:39:47 +0400

    first commit

--- Tpl/StudentLogic.cs
+++ Tpl/StudentLogic.cs
@@ -4,51 +4,86 @@ public static class StudentLogic
 {
     public static Task TaskCreated()
     {
-        throw new NotImplementedException();
+        return new Task(() => Console.Write("something"));
     }
 
     public static Task WaitingForActivation()
     {
-        throw new NotImplementedException();
+        var tcs = new TaskCompletionSource<object>();
+        return tcs.Task;
     }
 
     public static Task WaitingToRun()
     {
-        throw new NotImplementedException();
+        return Task.Factory.StartNew(
+       () =>
+       {
+           // Task does nothing here
+       },
+       CancellationToken.None,
+       TaskCreationOptions.None,
+       TaskScheduler.Default);
     }
 
     public static Task Running()
     {
-        throw new NotImplementedException();
+        var task = Task.Run(() =>
+        {
+            Console.WriteLine("Task is working");
+            Task.Delay(TimeSpan.FromSeconds(10)).Wait();
+        });
+
+        return task;
     }
 
     public static Task RanToCompletion()
     {
-        throw new NotImplementedException();
+        return Task.FromResult("Completed");
     }
 
     public static Task WaitingForChildrenToComplete()
     {
-        throw new NotImplementedException();
+        var childTask1 = Task.Run(() => Task.Delay(1000));
+        var childTask2 = Task.Run(() => Task.Delay(1000));
+
+        // Create a parent task that will wait for all child tasks to complete
+        var parentTask = Task.Run(() =>
+        {
+            return Task.WhenAll(childTask1, childTask2);
+        });
+
+        // Return the parent task
+        return parentTask;
     }
 
     public static Task IsCompleted()
     {
-        throw new NotImplementedException();
+        return Task.FromResult(true);
     }
 
     public static Task IsCancelled()
     {
-        throw new NotImplementedException();
+        using var cts = new CancellationTokenSource();
+        _ = cts.CancelAsync();
+        var task = Task.Delay(Timeout.Infinite, cts.Token);
+        return task;
     }
 
     public static Task IsFaulted()
     {
-        throw new NotImplementedException();
+        return Task.FromException(new InvalidOperationException("Faulted task"));
     }
 
     public static List<int> ForceParallelismPlinq()
     {
-        throw new NotImplementedException();
+        var testList = Enumerable.Range(1, 300).ToList();
+
+        // Use PLINQ to process the list in parallel
+        var result = testList
+            .AsParallel()
+            .Where(x => x > 0)
+            .OrderBy(x => Guid.NewGuid())
+            .ToList();
+        return result;
     }
 }
